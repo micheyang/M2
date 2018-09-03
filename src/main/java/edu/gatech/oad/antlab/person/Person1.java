85b9d52e2e72ea59d2b936a7440f2efa8f8b0368
Michelle Yang <michelleandschool@gmail.com>
2018-09-03 00:08:44 -0400

    Completed the method in the Person1 java file that rotates the letters of the passed in string two positions

--- src/main/java/edu/gatech/oad/antlab/person/Person1.java
+++ src/main/java/edu/gatech/oad/antlab/person/Person1.java
@@ -5,8 +5,8 @@ package edu.gatech.oad.antlab.person;
  *  returns their name and a
  *  modified string 
  *  
- *  @author Bob
- *  @version 1.1
+ *  @author Michelle Yang
+ *  @version 1.2
  */
 public class Person1 {
   /** Holds the persons real name */
@@ -30,8 +30,11 @@ public class Person1 {
 	 * @return the modified string
 	 */
 	private String calc(String input) {
-	  //Person 1 put your implementation here
-	  return null;
+		if (input.length() > 2) {
+			return input.substring(2, input.length()) + input.substring(0,2);
+		} else {
+			return input;
+		}
 	}
 	
 	/**
