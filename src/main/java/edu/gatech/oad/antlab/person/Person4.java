6d890260e87b6b899a6a1808544b261746d569bb
Michelle Tzeng <35272890+mtzeng471@users.noreply.github.com>
2018-09-07 09:19:12 -0400

    Update Person4.java

--- src/main/java/edu/gatech/oad/antlab/person/Person4.java
+++ src/main/java/edu/gatech/oad/antlab/person/Person4.java
@@ -31,11 +31,12 @@ public class Person4 {
      */
     private String calc(String input) {
       //Person 4 put your implementation here
-      String[] str2 = new String[input.length()];
-      for (int i = 0; i < input.length(); i++) {
-        str2[i] = str2[i] + 1;
-      }
-      return str2[str2.length - 1];
+      //String[] str2 = new String[input.length()];
+      //for (int i = 0; i < input.length(); i++) {
+        //str2[i] = str2[i] + 1;
+      //}
+      //return str2[str2.length - 1];
+      return input;
     }
     
     /**
